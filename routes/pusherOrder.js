398450e932122f4de5f2c236afd233a4b0abc6d4
Sanusi Mulyo <sanusiyagyoo@gmail.com>
2022-08-03 22:07:37 +0700

    Initial commit

--- routes/pusherOrder.js
+++ routes/pusherOrder.js
@@ -3,7 +3,7 @@ const pusher = require('../lib/pusher');
 module.exports = function (req, res, next) {
     if (req.body.orderType !== "Manual") {
         pusher.trigger("channel-order", "event-order", {
-            message: `New Order From Table ${req.body.tableName}`
+            message: `New Order from Table ${req.body.tableName}`
         });
     }
     next();
