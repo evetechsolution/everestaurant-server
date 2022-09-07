ecbfbcd47ea3469919f4415ddaa339645d39401a
Sanusi Mulyo <sanusiyagyoo@gmail.com>
2022-09-07 15:31:56 +0700

    Update cors socket

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const myServer = http.createServer(app);
 
 const io = new Server(myServer, {
     cors: {
-        origin: ["http://localhost:3060", "http://localhost:300", "https://everestaurant.vercel.app", "https://everestaurant-clientorder.vercel.app"],
+        origin: ["http://localhost:3060", "http://localhost:3000", "https://everestaurant.vercel.app", "https://everestaurant-clientorder.vercel.app"],
     },
 });
 
