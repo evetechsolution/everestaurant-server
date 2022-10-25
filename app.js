cb314bdcc61015512d739b353221ce65d2c7f3bd
Sanusi Mulyo <sanusiyagyoo@gmail.com>
2022-10-25 12:29:56 +0700

    disabled table view

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api/measurements', require('./routes/measurements'));
 app.use('/api/payments', require('./routes/payments'));
 app.use('/api/kitchens', require('./routes/kitchens'));
 app.use('/api/bars', require('./routes/bars'));
-app.use('/api/table-view', require('./routes/tableView'));
+// app.use('/api/table-view', require('./routes/tableView'));
 
 const myServer = http.createServer(app);
 
