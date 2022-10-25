e4dac69243cec70ad3f3a2fe3d4faad1d8a7c9b7
Sanusi Mulyo <sanusiyagyoo@gmail.com>
2022-10-25 12:50:23 +0700

    fix table view

--- routes/tableView.js
+++ routes/tableView.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Table = require('../models/tableview');
+const Table = require('../models/tableView');
 
 // GETTING ALL THE DATA
-// GET http://localhost:5000/api/table/
+// GET http://localhost:5000/api/table-view/
 router.get('/', async (req, res) => {
     try {
         const listofData = await Table.find().sort({ "id": 1 });
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 });
 
 // CREATE NEW DATA
-// POST http://localhost:5000/api/table/create
+// POST http://localhost:5000/api/table-view/create
 router.post('/create', async (req, res) => {
     try {
         const data = new Table(req.body);
@@ -26,7 +26,7 @@ router.post('/create', async (req, res) => {
 });
 
 // GET A SPECIFIC DATA
-// GET http://localhost:5000/api/table/:id
+// GET http://localhost:5000/api/table-view/:id
 router.get('/:id', async (req, res) => {
     try {
         const spesificData = await Table.findById(req.params.id);
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // UPDATE A SPECIFIC DATA
-// PATCH http://localhost:5000/api/table/:id
+// PATCH http://localhost:5000/api/table-view/:id
 router.patch('/:id', async (req, res) => {
     try {
         const updatedData = await Table.updateOne(
@@ -53,23 +53,23 @@ router.patch('/:id', async (req, res) => {
 });
 
 // UPDATE A SPECIFIC DATA BY NUMBER
-// PATCH http://localhost:5000/api/table/update/:id
-// router.patch('/update/:id', async (req, res) => {
-//     try {
-//         const updatedData = await Table.updateOne(
-//             { number: req.params.id },
-//             {
-//                 $set: req.body
-//             }
-//         );
-//         res.json(updatedData);
-//     } catch (err) {
-//         res.json({ message: err });
-//     }
-// });
+// PATCH http://localhost:5000/api/table-view/update/:id
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const updatedData = await Table.updateOne(
+            { number: req.params.id },
+            {
+                $set: req.body
+            }
+        );
+        res.json(updatedData);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
 
 // DELETE A SPECIFIC DATA
-// DELETE http://localhost:5000/api/table/:id
+// DELETE http://localhost:5000/api/table-view/:id
 router.delete('/:id', async (req, res) => {
     try {
         const deletedData = await Table.deleteOne({ _id: req.params.id });
